571a994b421dd3b77494d0bc74f63ba9454c2d73
Ishrz <ishrzz66@gmail.com>
2026-01-26 22:28:44 +0530

    pending

--- routing/index.js
+++ routing/index.js
@@ -14,10 +14,11 @@ app.get('/user',(req,res)=>{
     res.send([25,26])
 })
 
-app.post('/user/1',(req,res)=>{
+app.post('/user/1:name1:name2',(req,res)=>{
     // const data=JSON.parse(req.body)
     //  console.log(req.body)
     const {name}=req.body
+    
     const message=  `Hello "${name}" your data is recieved`
     res.status(200).send(message)
 })
