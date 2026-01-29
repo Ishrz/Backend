8b15fcab54f106fe9ab95fdaba80c17ebb97bcdb
Ishrz <ishrzz66@gmail.com>
2026-01-30 00:31:41 +0530

    test

--- MiddleWares/src/index.js
+++ MiddleWares/src/index.js
@@ -1,24 +1,32 @@
-const exress=require('express')
+const express=require('express')
+
 
 const app=express()
 
+app.use(express.json())
 
 app.get("/user",(req,res)=>{
-
+        const {name,role,isPaid} = req.body
+        console.log(name,role,isPaid)
+        if(!isPaid){
+            res.json(`Hello ${name} you havent paid till now your ${role} is not approved `)
+        }else{
+            res.json(`Hello ${name} your ${role} is approved`)
+        }
 })
 
 
-app.post("/user",(req,res)=>{
+// app.post("/user",(req,res)=>{
 
-})
+// })
 
-app.delete("/user",(req,res)=>{
+// app.delete("/user",(req,res)=>{
 
-})
+// })
 
-app.patch("/user",(req,res)=>{
+// app.patch("/user",(req,res)=>{
 
-})
+// })
 
 
 module.exports=app
