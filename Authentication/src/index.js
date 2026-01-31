73396748c597f0080dfaaf944d48f918c328217c
Ishrz <ishrzz66@gmail.com>
2026-02-01 02:42:59 +0530

    created token genrator and added it on user data whoever signup

--- Authentication/src/index.js
+++ Authentication/src/index.js
@@ -6,6 +6,14 @@ app.use(express.json())
 
 const allUsers=[]
 
+const getToken = ()=>{
+    const options=["A","B","C","D","a","b","c","d",1,2,3,4]
+    let token=''
+    for(let i=0; i<options.length;i++)
+    token +=options[Math.floor(Math.random()*options.length)]
+
+    return token
+}
 
 app.post("/signin",(req,res)=>{
     const {username,password} = req.body
@@ -16,25 +24,33 @@ app.post("/signin",(req,res)=>{
     })
 
     console.log(allUsers)
-    res.status(201).json("Congratulation your successfully signed-in")
+    res.status(201).json({
+        message:"Congratulation your successfully signed-in",
+        data:{username,password}
+    })
 })
 
 app.post("/signup",(req,res)=>{
         const {username,password} = req.body
 
-        if(allUsers.find(u=> (u.user === username && u.password === password))){
+        const currentUser=allUsers.find(u=> (u.user === username && u.password === password))
 
-                console.log(allUsers)
+            if(currentUser){
+                console.log("before Token",allUsers)
+                const token=getToken()
+                currentUser.token=token
+                res.json({
+                    message:"authentication done heres your new data",
+                    data:currentUser
+                })
+            }else{
+                console.log("authentication failed")
 
+                console.log("from failed auth",allUsers)
 
-            res.json("your are a authenticate person")
-        }else{
-
-                console.log(allUsers)
-
-
-            res.json("you are not authenticate person")
+            res.json({message:"your are not authenticate"})
         }
+        console.log("from end of post method code: ",allUsers)
 })
 
 
