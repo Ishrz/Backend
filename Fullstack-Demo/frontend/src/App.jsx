537bb46298746ebebe4d826ca53fdc348998bfc4
Ishrz <ishrzz66@gmail.com>
2026-02-05 21:57:33 +0530

    integrated delete api and edit on reactapp and get api for getting notes from database

--- Fullstack-Demo/frontend/src/App.jsx
+++ Fullstack-Demo/frontend/src/App.jsx
@@ -5,10 +5,21 @@ import "./App.css";
 function App() {
   const [notes, setNotes] = useState([]);
 
-  const btnClick =async ()=>{
+  const fetchNotes =async ()=>{
     const axData=await axios.get("http://127.0.0.1:3000/notes")
-    // console.log(axData)
+    console.log(axData)
     setNotes(axData.data.notes)
+  }
+
+  const handleDelete=async(id)=>{
+    console.log(id)
+    const response=await axios.delete(`http://127.0.0.1:3000/notes/${id}`)
+    fetchNotes()
+    console.log(response)
+  }
+
+  const handleEdit=async ()=>{
+    
   }
 
   return (
@@ -18,11 +29,15 @@ function App() {
           <div id="note">
             <h1>{note.title}</h1>
             <h2>{note.description}</h2>
+            <div id="btnsDiv">
+              <button id="btn2" onClick={()=>handleDelete(note._id)}>Delete</button>
+              <button id="btn" onClick={()=>handleEdit(note._id)}>Edit</button>
+            </div>
           </div>
         ))}
       </div>
       <div>
-        <button id="btn" onClick={btnClick}>
+        <button id="btn" onClick={fetchNotes}>
           Get Notes
         </button>
       </div>
