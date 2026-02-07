ce7af4642af81ea3cfe30661593b181a45cbb41c
Ishrz <ishrzz66@gmail.com>
2026-02-08 01:32:36 +0530

    added login endpoint with hashing

--- AuthnHashing/src/routes/auth.route.js
+++ AuthnHashing/src/routes/auth.route.js
@@ -49,14 +49,28 @@ authRouter.post("/register", async(req,res)=>{
 })
 
 
-authRouter.post("/login", (req,res)=>{
+authRouter.post("/login",async (req,res)=>{
     const {email,password} = req.body
 
-    const isUserExist = userModel.find({email})
-
-    if(isUserExist[0]?.email !== email){
+    if(!password) return res.json({message:"password field is empty"})
+    const isUserExist =await userModel.findOne({email})
+   
+    if(!isUserExist){
         return res.json({
             message:"user not found"
+        })
+    }
+
+    const loginPassword = crypto.createHash("md5").update(password).digest("hex")
+    console.log("login pass hash :",loginPassword)
+    if(isUserExist.password === loginPassword && isUserExist.email === email ){
+        res.json({
+            message:"login successfull...",
+            loginUser: isUserExist.name
+        })
+    }else{
+        return res.json({
+            message:"inValid Caredentials"
         })
     }
 })
