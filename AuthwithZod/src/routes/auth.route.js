1f045efeedc40eb1c8e5e27fd318a33b19aa4de7
Ishrz <ishrzz66@gmail.com>
2026-02-10 17:05:17 +0530

    WIP: using Bcrypt for hashing and salting at registration

--- AuthwithZod/src/routes/auth.route.js
+++ AuthwithZod/src/routes/auth.route.js
@@ -1,11 +1,33 @@
 const express=require("express")
-const userModel= require("../models")
+const userModel= require("../models/user.model")
+const bcrypt = require("bcrypt")
 
 const authRouter=express.Router()
 
 
 authRouter("/register", async (req,res)=>{
 
+    const {name,email,password} = req.body
+
+    if(!name) return res.json("please enter name")
+    if(!email) return res.json("email is required")
+    if(!password) return res.json("password is required")
+
+    try{
+    const userCheck=await userModel.findOne({email})
+
+    if(userCheck?.email == email) return res.json(`User already registered with this email id :${email}`) 
+    
+    const user = userModel.create({
+        name,
+        email,
+        password:
+    })
+
+    }catch(err){
+        res.json({message:`something went wrong, error :${err}`})
+        console.log(`Error : ${err}`)
+    }
 
 
 })
